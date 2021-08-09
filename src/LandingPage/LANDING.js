76b430afe2f58ace1343d10383d1d544569d03c7
Vishnurr2k01 <vishnurr2k01@gmail.com>
2021-08-09 19:40:57 +0530

    tailwind-setup

---
+++ src/LandingPage/LANDING.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LANNDING = () => {
+  return (
+    <div>
+      <h3>home page</h3>
+    </div>
+  );
+};
+export default LANNDING;
